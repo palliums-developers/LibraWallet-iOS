e079633a0ace6c3b54c831e3bd915379444192a1
YingDong Wang <wyd@palliums.org>
2019-08-29 14:44:25 +0800

    Initial Commit

---
+++ LibraWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LibraWallet
+//
+//  Created by palliums on 2019/8/29.
+//  Copyright © 2019 palliums. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
