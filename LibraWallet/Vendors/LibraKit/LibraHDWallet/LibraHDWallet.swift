e237a589606ca58ece1f155aba65652a507cba42
Yingdong Wang <wangyingdong@palliums.org>
2020-07-31 11:19:28 +0800

    为了政治正确修改命名

--- LibraWallet/Vendors/LibraKit/LibraHDWallet/LibraHDWallet.swift
+++ LibraWallet/Vendors/LibraKit/LibraHDWallet/LibraHDWallet.swift
@@ -44,7 +44,7 @@ public final class LibraHDWallet {
         let tempInfo = Data() + Array("LIBRA WALLET: derived key$".utf8) + depthData.bytes
         do {
             let privateKey = try HKDF.init(password: seed,
-                                     salt:Array("LIBRA WALLET: master key salt$".utf8),
+                                     salt:Array("LIBRA WALLET: main key salt$".utf8),
                                      info: tempInfo.bytes,
                                      keyLength: 32,
                                      variant: .sha3_256).calculate()
@@ -55,7 +55,7 @@ public final class LibraHDWallet {
     }
     func getMasterKey() throws -> Data {
         do {
-            let masterKey = try HMAC.init(key: "LIBRA WALLET: master key salt$", variant: .sha3_256).authenticate(seed)
+            let masterKey = try HMAC.init(key: "LIBRA WALLET: main key salt$", variant: .sha3_256).authenticate(seed)
             return Data.init(bytes: masterKey, count: masterKey.count)
 
         } catch {
