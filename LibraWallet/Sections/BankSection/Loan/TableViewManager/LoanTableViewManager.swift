ce7957756638622c4d5f83e2d19c8917628cb762
Yingdong Wang <wangyingdong@palliums.org>
2020-08-26 17:01:32 +0800

    添加贷款页面UI

---
+++ LibraWallet/Sections/BankSection/Loan/TableViewManager/LoanTableViewManager.swift
@@ -0,0 +1,128 @@
+//
+//  LoanTableViewManager.swift
+//  LibraWallet
+//
+//  Created by wangyingdong on 2020/8/26.
+//  Copyright © 2020 palliums. All rights reserved.
+//
+
+import UIKit
+
+class LoanTableViewManager: NSObject {
+    weak var delegate: AssetsPoolTransactionsTableViewManagerDelegate?
+    var dataModels: [Int]? = [1, 2, 3]
+    deinit {
+        print("DepositTableViewManager销毁了")
+    }
+}
+extension LoanTableViewManager: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 48
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        guard let model = self.dataModels else {
+            return
+        }
+        //        self.delegate?.tableViewDidSelectRowAtIndexPath(indexPath: indexPath, model: model[indexPath.row])
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 140
+        } else {
+            return 10
+        }
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 {
+            let identifier = "Header"
+            if let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: identifier) {
+                return header
+            } else {
+                let header = LoanTableViewHeaderView.init(reuseIdentifier: identifier)
+                return header
+            }
+        } else {
+            let view = UIView.init()
+            view.backgroundColor = UIColor.init(hex: "F7F7F9")
+            return view
+        }
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.001
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let view = UIView.init()
+        view.backgroundColor = UIColor.init(hex: "F7F7F9")
+        return view
+    }
+}
+extension LoanTableViewManager: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 3//dataModels?.count ?? 0
+        } else {
+            return 1
+        }
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let identifier = "NormalCell"
+            if let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as? LoanTableViewCell {
+                if let data = dataModels, data.isEmpty == false {
+                    //                cell.model = data[indexPath.row]
+                    cell.hideSpcaeLineState = (data.count - 1) == indexPath.row ? true:false
+                }
+                //            cell.indexPath = indexPath
+                cell.selectionStyle = .none
+                return cell
+            } else {
+                let cell = LoanTableViewCell.init(style: UITableViewCell.CellStyle.default, reuseIdentifier: identifier)
+                if let data = dataModels, data.isEmpty == false {
+                    //                cell.model = data[indexPath.row]
+                    cell.hideSpcaeLineState = (data.count - 1) == indexPath.row ? true:false
+                }
+                cell.selectionStyle = .none
+                return cell
+            }
+        } else if indexPath.section == 1 {
+            let identifier = "DescribeCell"
+            if let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as? LoanDescribeTableViewCell {
+                if let data = dataModels, data.isEmpty == false {
+                    //                cell.model = data[indexPath.row]
+                }
+                //            cell.indexPath = indexPath
+                cell.selectionStyle = .none
+                return cell
+            } else {
+                let cell = LoanDescribeTableViewCell.init(style: UITableViewCell.CellStyle.default, reuseIdentifier: identifier)
+                if let data = dataModels, data.isEmpty == false {
+                    //                cell.model = data[indexPath.row]
+                }
+                cell.selectionStyle = .none
+                return cell
+            }
+        } else {
+            let identifier = "QuestionCell"
+            if let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as? LoanQuestionTableViewCell {
+                if let data = dataModels, data.isEmpty == false {
+                    //                cell.model = data[indexPath.row]
+                }
+                //            cell.indexPath = indexPath
+                cell.selectionStyle = .none
+                return cell
+            } else {
+                let cell = LoanQuestionTableViewCell.init(style: UITableViewCell.CellStyle.default, reuseIdentifier: identifier)
+                if let data = dataModels, data.isEmpty == false {
+                    //                cell.model = data[indexPath.row]
+                }
+                cell.selectionStyle = .none
+                return cell
+            }
+        }
+        
+    }
+}
---
+++ LibraWallet/Sections/BankSection/Loan/View/LoanView.swift
@@ -0,0 +1,91 @@
+//
+//  LoanView.swift
+//  LibraWallet
+//
+//  Created by wangyingdong on 2020/8/26.
+//  Copyright © 2020 palliums. All rights reserved.
+//
+
+import UIKit
+
+class LoanView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(tableView)
+        addSubview(footerBackgroundView)
+        footerBackgroundView.addSubview(confirmButton)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    deinit {
+        print("LoanView销毁了")
+    }
+    //MARK: - 布局
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalTo(self)
+            make.left.right.equalTo(self)
+            make.bottom.equalTo(footerBackgroundView.snp.top)
+        }
+        footerBackgroundView.snp.makeConstraints { (make) in
+            make.left.right.equalTo(self)
+            make.bottom.equalTo(self)
+            make.height.equalTo(128)
+        }
+        confirmButton.snp.makeConstraints { (make) in
+            make.top.equalTo(footerBackgroundView).offset(20)
+            make.left.equalTo(footerBackgroundView).offset(69)
+            make.right.equalTo(footerBackgroundView.snp.right).offset(-69)
+            make.height.equalTo(40)
+        }
+    }
+    //MARK: - 懒加载对象
+    lazy var tableView: UITableView = {
+        let tableView = UITableView.init(frame: CGRect.zero, style: UITableView.Style.grouped)
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        tableView.estimatedRowHeight = 0;
+        tableView.estimatedSectionHeaderHeight = 0;
+        tableView.backgroundColor = UIColor.init(hex: "F7F7F9")
+        tableView.register(LoanTableViewCell.classForCoder(), forCellReuseIdentifier: "CellNormal")
+        tableView.register(LoanTableViewHeaderView.classForCoder(), forHeaderFooterViewReuseIdentifier: "Header")
+        return tableView
+    }()
+    lazy var footerBackgroundView: UIView = {
+        let footer = UIView.init()
+        footer.backgroundColor = UIColor.white
+        return footer
+    }()
+    lazy var confirmButton: UIButton = {
+        let button = UIButton.init(type: UIButton.ButtonType.custom)
+        button.setTitle(localLanguage(keyString: "wallet_bank_loan_button_title"), for: UIControl.State.normal)
+        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: adaptFont(fontSize: 15), weight: UIFont.Weight.regular)
+        button.addTarget(self, action: #selector(buttonClick(button:)), for: UIControl.Event.touchUpInside)
+        let width = UIScreen.main.bounds.width - 69 - 69
+        button.layer.insertSublayer(colorGradualChange(size: CGSize.init(width: width, height: 40)), at: 0)
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        return button
+    }()
+    @objc func buttonClick(button: UIButton) {
+        //        self.delegate?.confirm()
+    }
+    var toastView: ToastView? {
+        let toast = ToastView.init()
+        return toast
+    }
+    func deleteRowInTableView(indexPaths: [IndexPath]) {
+        
+    }
+    func insertRowInTableView(indexPaths: [IndexPath]) {
+        
+    }
+    func reloadRowInTableView(indexPaths: [IndexPath]) {
+        
+    }
+    func dealErrorToast(error: LibraWalletError) {
+        
+    }
+}
