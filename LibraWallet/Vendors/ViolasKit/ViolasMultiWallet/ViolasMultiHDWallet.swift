2426d5f601a3e02e3547112bbf84607560281110
Yingdong Wang <wangyingdong@palliums.org>
2021-02-22 15:58:29 +0800

    fix: 修复ViolasKit失效问题

--- LibraWallet/Vendors/ViolasKit/ViolasMultiWallet/ViolasMultiHDWallet.swift
+++ LibraWallet/Vendors/ViolasKit/ViolasMultiWallet/ViolasMultiHDWallet.swift
@@ -48,7 +48,7 @@ struct ViolasMultiHDWallet {
             let tempInfo = Data() + Array("DIEM WALLET: derived key$".utf8) + depthData.bytes
             do {
                 let privateKey = try HKDF.init(password: model.seed,
-                                         salt:Array("DIEM WALLET: master key salt$".utf8),
+                                         salt:Array("DIEM WALLET: main key salt$".utf8),
                                          info: tempInfo.bytes,
                                          keyLength: 32,
                                          variant: .sha3_256).calculate()
